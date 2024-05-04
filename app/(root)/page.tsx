b77c28d0d9a516105b959e70cf0dff21b039dc2d
Yohannes <jhon.klaus2008@gmail.com>
2024-05-04 17:00:07 +0300

    implement rightside bar for bank cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: "John" };
+  const loggedIn = {
+    firstName: "John",
+    lastName: "Doe",
+    email: "johnm@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -19,7 +24,13 @@ const Home = () => {
             totalCurrentBalance={1250.25}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 125.5 }, { currentBalance: 550.5 }]}
+      />
     </section>
   );
 };
